a92808aab22dbbba5c55380185948aae9103e199
renquanboMacbookPro <renquanbo7453@gmail.com>
2021-09-28 12:09:09 +1300

    add jwt config

--- src/main/java/com/breadcrumbdata/locationengineserver/config/SecurityConfig.java
+++ src/main/java/com/breadcrumbdata/locationengineserver/config/SecurityConfig.java
@@ -1,19 +1,33 @@
 package com.breadcrumbdata.locationengineserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.security.interfaces.RSAPublicKey;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${jwt.public.key}")
+    RSAPublicKey key;
+
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder(11);
@@ -21,6 +35,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
+
+        http.csrf((csrf) -> csrf.ignoringAntMatchers("/token"))
+                .httpBasic(Customizer.withDefaults())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling((exceptions) -> exceptions
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+                );
+
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/sign-up").permitAll()
@@ -39,5 +63,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(this.key).build();
     }
 }
---
+++ src/main/java/com/breadcrumbdata/locationengineserver/controller/TokenController.java
@@ -0,0 +1,55 @@
+package com.breadcrumbdata.locationengineserver.controller;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.time.Instant;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TokenController {
+    @Value("${jwt.private.key}")
+    RSAPrivateKey key;
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        Instant now = Instant.now();
+        long expiry = 36000L;
+        // @formatter:off
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JWTClaimsSet claims = new JWTClaimsSet.Builder()
+                .issuer("self")
+                .issueTime(new Date(now.toEpochMilli()))
+                .expirationTime(new Date(now.plusSeconds(expiry).toEpochMilli()))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+        // @formatter:on
+        JWSHeader header = new JWSHeader.Builder(JWSAlgorithm.RS256).build();
+        SignedJWT jwt = new SignedJWT(header, claims);
+        return sign(jwt).serialize();
+    }
+
+    SignedJWT sign(SignedJWT jwt) {
+        try {
+            jwt.sign(new RSASSASigner(this.key));
+            return jwt;
+        }
+        catch (Exception ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+}
